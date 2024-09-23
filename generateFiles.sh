e67598a2786d917ac9b84a9dbbc499d888a64d78
Ian Mitchell <56634530+iangmitchell@users.noreply.github.com>
2024-09-23 17:48:23 +0100

    Update generateFiles.sh

--- generateFiles.sh
+++ generateFiles.sh
@@ -4,3 +4,9 @@ echo -n 100 1 > 0.txt
 echo -n 200 2 > 1.txt
 echo -n 400 2 > 2.txt
 echo -n 800 1 > 3.txt
+echo -n 150 3 > 4.txt
+echo -n 250 4 > 5.txt
+echo -n 600 4 > 6.txt
+echo -n 750 3 > 7.txt
+echo -n 850 5 > 8.txt
+echo -n 900 8 > 9.txt
